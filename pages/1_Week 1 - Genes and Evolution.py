d5e43538a0068bd4d9611149cb9e546f3c0d007f
LucMahieu <lucmauricemahieu@gmail.com>
2023-10-11 21:13:59 +0200

    Progress bar update

--- pages/1_Week 1 - Genes and Evolution.py
+++ pages/1_Week 1 - Genes and Evolution.py
@@ -23,7 +23,7 @@ main_container = st.container()
 sub_container = st.container()
 
 # Navigate to next question when button is pressed
-if st.session_state.card_index <= len(questions) - 1:
+if st.session_state.card_index < len(questions) - 1:
     # Two columns in which the flashcard navigation buttons are placed
     col1, col2 = sub_container.columns(2)
     with col1:
