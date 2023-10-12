a2cfcce418933f20a9c5a099881a30c6c3d63bca
LucMahieu <lucmauricemahieu@gmail.com>
2023-10-12 16:40:32 +0200

    Overview aangepast met betere info en layout.

--- Overview.py
+++ Overview.py
@@ -8,20 +8,30 @@ st.set_page_config(
 st.title("LearnLoop")
 st.subheader("**Excited for you to be one of the first users of LearnLoop!** ")
 st.write(""" 
-We crafted LearnLoop to help you swiftly and efficiently comprehend subjects from your study materials. This is the first prototype, so it only has the bare minimum features and there might be some errors. With your feedback and that of others, we will determine what features to build next. 
+We crafted LearnLoop to help you swiftly and efficiently comprehend subjects from your study materials. It will do this by **automatically transforming your study materials into a more fun and effective learning pathway**. This is the first prototype, so it only contains pre-generated quizzes from one subject and these were manually checked for accuracy by fellow students. With your feedback and that of others, we will determine how to improve the algoritms that drive LearnLoop and what features to build next.
+""")
 
-**Why use LearnLoop?** \n
+# Create two columns
+col1, col2 = st.columns(2)
+
+# Write into column 1
+col1.subheader("**Why use LearnLoop?**")
+col1.write(""" 
 📚 All concepts clearly organized in one place. \n
 ⏱️ Study effectively by testing yourself. \n
+""")
 
-**Upcoming features:** \n
+# Write into column 2
+col2.subheader("**Upcoming features:**")
+col2.write(""" 
 📖 Transform personal notes into a customized learning pathway. \n
 🎮 A gamified learning experience. \n
 📈 Learning pathway tailored to your learning curve. \n
-\n
-**See the potential?** A voluntary donation goes a long way. It not only fuels our progress but also signifies how many students want LearnLoop to keep growing and improving.
-"""
-         )
+""")
+
+st.write("""
+**See the potential?** Your donation signals to us that there's a demand for LearnLoop to continue evolving and improving. Every contribution will be directly channeled into the development of new features and the enhancement of our algorithms.
+""")
 
 st.subheader("**Voluntary Donation options:**")
 st.write("""€1,00: http://bit.ly/3PPHysV \n
