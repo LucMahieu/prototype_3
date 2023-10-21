6886bd60cb4226a8743985b6b14eae9ad6543e9c
LucMahieu <lucmauricemahieu@gmail.com>
2023-10-21 10:24:41 +0200

    SR test pagina's om te testen of meerdere users tegelijk kunnen.

---
+++ pages/15_test_1.py
@@ -0,0 +1,112 @@
+import streamlit as st
+
+solid_list_questions = ['t1', 't2', 't3', 't4', 't5']
+solid_list_answers = ['1', '2', '3', '4', '5']
+
+def change_card_index(index):
+    # Select first element and re-insert at index
+    st.session_state.questions.insert(index, st.session_state.questions[0])
+    st.session_state.answers.insert(index, st.session_state.answers[0])
+
+    # Delete the first duplicate card
+    del st.session_state.questions[0]
+    del st.session_state.answers[0]
+
+    return
+
+def evaluate_graduation(current_card):
+    if current_card in st.session_state.easy_count:
+        st.session_state.easy_count[current_card] += 1
+    else:
+        st.session_state.easy_count[current_card] = 1
+
+    # Delete card if graduated
+    if st.session_state.easy_count[current_card] >= 2:
+        del st.session_state.questions[0]
+        del st.session_state.answers[0]
+    else:
+        change_card_index(20)
+
+    return
+
+
+def reset_easy_count(current_card):
+    st.session_state.easy_count[current_card] = 0
+
+
+# -------------------------SESSION STATES----------------------------- #
+
+if 'easy_count' not in st.session_state:
+    st.session_state.easy_count = {}
+
+if 'questions' not in st.session_state:
+    st.session_state.questions = ['kaart1', 'kaart2', 'kaart3', 'kaart4', 'kaart5']
+
+if 'answers' not in st.session_state:
+    st.session_state.answers = ['1', '2', '3', '4', '5']
+
+if 'show_answer' not in st.session_state:
+    st.session_state.show_answer = False
+
+if 'previous_page_name' not in st.session_state:
+    st.session_state.previous_page_name = None
+
+if 'current_page_name' not in st.session_state:
+    st.session_state.current_page_name = __file__
+
+# -------------------------------MAIN---------------------------------- #
+def initialise_page_lists():
+    st.session_state.questions = solid_list_questions.copy()
+    st.session_state.answers = solid_list_answers.copy()
+    return
+
+# Read and store current file name
+st.session_state.current_page_name = __file__
+st.write(__file__)
+
+# Check if a new page is opened
+if st.session_state.current_page_name != st.session_state.previous_page_name:
+    # Change lists in session state with current week lists
+    initialise_page_lists()
+    st.session_state.previous_page_name = st.session_state.current_page_name
+
+
+card_progress = st.progress(0)
+main_container = st.container()
+
+with main_container:
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        if st.button('Easy', use_container_width=True):
+            # Count executive times the user found current card easy
+            evaluate_graduation(st.session_state.questions[0])
+            st.session_state.show_answer = False
+
+    with col2:
+        if st.button('Medium', use_container_width=True):
+            st.session_state.show_answer = False
+            reset_easy_count(st.session_state.questions[0])
+            change_card_index(5)
+
+    with col3:
+        if st.button('Hard', use_container_width=True):
+            st.session_state.show_answer = False
+            reset_easy_count(st.session_state.questions[0])
+            change_card_index(2)
+
+
+if st.button('Show Answer', use_container_width=True):
+    st.session_state.show_answer = not st.session_state.show_answer
+
+if len(st.session_state.questions) == 0:
+    st.session_state.questions = solid_list_questions.copy()
+    st.session_state.answers = solid_list_answers.copy()
+    st.session_state.easy_count = {}
+
+st.subheader(st.session_state.questions[0])
+if st.session_state.show_answer:
+    st.write(st.session_state.answers[0])
+
+card_progress.progress(int(sum(st.session_state.easy_count.values()) / (2 * len(solid_list_questions)) * 100))
+
+
